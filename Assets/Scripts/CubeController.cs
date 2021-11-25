d77d54ac8c0d2f0f09bb445e461f5f983b7699df
Riukko <darkarceus10@hotmail.fr>
2021-11-25 12:14:53 +0100

    Functional eye rotation and tracking toward the target object with constraints on X and Y axis (it's a bit uncanny but it's fine)
    TO DO :
    - Inverse kinematic bones setup
    - Legs movements according to distance to target object
    - Arm movements according to distance to target object

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
+
+    public float yScrollSpeed = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,11 @@ public class CubeController : MonoBehaviour
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = 2;
         mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-        transform.position = mousePos;
+        transform.position = new Vector3(mousePos.x, transform.position.y, mousePos.z);
+
+        if(Input.mouseScrollDelta.y != 0)
+        {
+            transform.position += Input.mouseScrollDelta.y < 0 ? new Vector3(0, -yScrollSpeed , 0) : new Vector3(0, yScrollSpeed, 0); 
+        }
     }
 }
--- Assets/Scripts/OtterController.cs
+++ Assets/Scripts/OtterController.cs
@@ -20,10 +20,16 @@ public class OtterController : MonoBehaviour
     [SerializeField] float rightEyeMaxYRotation;
     [SerializeField] float rightEyeMinYRotation;
 
+    [SerializeField] float leftEyeMaxXRotation;
+    [SerializeField] float leftEyeMinXRotation;
+    [SerializeField] float rightEyeMaxXRotation;
+    [SerializeField] float rightEyeMinXRotation;
+
 
     void LateUpdate()
     {
         HeadTrackingUpdate();
+        EyeTrackingUpdate();
     }
 
     void HeadTrackingUpdate()
@@ -60,11 +66,13 @@ public class OtterController : MonoBehaviour
 
     void EyeTrackingUpdate()
     {
+        //Set up the rotation we want the eyes to have to look at the target object
         Quaternion targetEyeRotation = Quaternion.LookRotation(
-          targetTransform.position - headBone.position, // toward target
+          headBone.position - targetTransform.position, 
           transform.up
         );
 
+        //Slerps to smooth out the movement of the eyes
         leftEye.rotation = Quaternion.Slerp(
           leftEye.rotation,
           targetEyeRotation,
@@ -76,6 +84,68 @@ public class OtterController : MonoBehaviour
           targetEyeRotation,
           1 - Mathf.Exp(-eyeTrackingSpeed * Time.deltaTime)
         );
+
+
+        //We're clamping the rotation values only on the y and x axis, therefore we can work with Euler angles
+        float leftEyeCurrentYRotation = leftEye.localEulerAngles.y;
+        float rightEyeCurrentYRotation = rightEye.localEulerAngles.y;
+
+        float leftEyeCurrentXRotation = leftEye.localEulerAngles.x;
+        float rightEyeCurrentXRotation = rightEye.localEulerAngles.x;
+
+        // Clamp the rotation to be between -180 and 180
+        if (leftEyeCurrentYRotation > 180)
+        {
+            leftEyeCurrentYRotation -= 360;
+        }
+        if (rightEyeCurrentYRotation > 180)
+        {
+            rightEyeCurrentYRotation -= 360;
+        }
+
+        if (leftEyeCurrentXRotation > 180)
+        {
+            leftEyeCurrentXRotation -= 360;
+        }
+        if (rightEyeCurrentXRotation > 180)
+        {
+            rightEyeCurrentXRotation -= 360;
+        }
+
+        // Clamp the Y axis rotation according to the Min/Max Y rotation defined in the class attributes
+        float leftEyeClampedYRotation = Mathf.Clamp(
+            leftEyeCurrentYRotation,
+            leftEyeMaxYRotation,
+            leftEyeMinYRotation
+        );
+        float rightEyeClampedYRotation = Mathf.Clamp(
+            rightEyeCurrentYRotation,
+            rightEyeMinYRotation,
+            rightEyeMaxYRotation
+        );
+
+        float leftEyeClampedXRotation = Mathf.Clamp(
+            leftEyeCurrentXRotation,
+            leftEyeMinXRotation,
+            leftEyeMaxXRotation);
+
+        float rightEyeClampedXRotation = Mathf.Clamp(
+            rightEyeCurrentXRotation,
+            rightEyeMinXRotation,
+            rightEyeMaxXRotation);
+
+        // Apply the clamped Y rotation without changing the X and Z rotations
+        leftEye.localEulerAngles = new Vector3(
+            leftEyeClampedXRotation,
+            leftEyeClampedYRotation,
+            0
+        );
+        rightEye.localEulerAngles = new Vector3(
+            rightEyeClampedXRotation,
+            rightEyeClampedYRotation,
+            0
+        );
+
     }
 }
 
